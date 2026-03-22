b3ca30f82e8e48522f234c50ccf8f9f7c4096907
Aryan Kaushik <aryankaushik541@gmail.com>
2026-03-22 20:50:15 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./components/footer";
+import Header from "./components/header";
+import Home from "./pages/home";
+import Gallery from "./pages/gallery"; 
+import Aminities from "./pages/aminities";
+function App() {
+  return (
+    <BrowserRouter>
+<Header/>
+         <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/hotels" element={<h1>All Hotels Page</h1>} />
+          <Route path="/casino-hotels" element={<h1>Casino Hotels</h1>} />
+          <Route path="/extended-stay" element={<h1>Extended Stay Hotels</h1>} />
+           <Route path="/resorts" element={<h1>Resorts</h1>} />
+           <Route path="/gallery" element={<Gallery />} /> 
+           <Route path="/aminities" element={<Aminities />} /> 
+         </Routes>
+
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
