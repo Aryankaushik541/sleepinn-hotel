4ed1f4cd37a4f6596e65764900a438c7fb7c25b4
Aryan Kaushik <aryankaushik541@gmail.com>
2026-03-22 21:11:58 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Footer from "./components/footer";
 import Header from "./components/header";
 import Home from "./pages/home";
 import Gallery from "./pages/gallery"; 
@@ -18,7 +17,7 @@ function App() {
            <Route path="/aminities" element={<Aminities />} /> 
          </Routes>
 
-      <Footer />
+      
     </BrowserRouter>
   );
 }
